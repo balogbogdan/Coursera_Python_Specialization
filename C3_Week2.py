653c99a8f21f3ad3465be5032538cd055c054d6e
Bogdan Balog <bogdanbalog@outlook.com>
2018-03-27 13:51:47 +0300

    created the C3_Week2 .py file

---
+++ C3_Week2.py
@@ -0,0 +1,21 @@
+"""
+Regular expresions:
+
+^  Matches the beginning of a line
+$  Matches the end of the line
+.  Matches any character
+\s Matches whitespace
+\S Matches any non-whitespace character
+
+* Repeats a char zero or more times
+*? Repeats a char zero or more times (non-greedy)
++ Repeats a char one or more times
++? Repeats a char one or more times (non-greedy)
+
+[aeiou] Matches a single char in the listed set
+[^XYZ} Matches a single char not in the listed set
+[a-z0-9] The set of chars can include a range
+( Indicates where string extraction is to start
+) Indicates where string extraction is to end
+
+"""
